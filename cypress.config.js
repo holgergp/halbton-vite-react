29fa54129aec04f4320fe271576356f2ab4b6945
Holger Grosse-Plankermann <holgergp@gmail.com>
2024-06-25 12:19:53 +0200

    fix(cypress): we do not need support files

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
 
-    baseUrl: 'http://localhost:5173',
+    baseUrl: "http://localhost:5173",
+    supportFile: false,
   },
-})
+});
